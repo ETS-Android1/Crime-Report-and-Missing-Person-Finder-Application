8e505ae8070318eba6f0f4b6cc0dd9d3840fc950
Rohitreddz <77590158+Rohitreddz@users.noreply.github.com>
2021-10-18 18:55:04 -0500

    created project and added dashboard buttons

---
+++ app/src/main/java/com/example/crmp/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.crmp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
