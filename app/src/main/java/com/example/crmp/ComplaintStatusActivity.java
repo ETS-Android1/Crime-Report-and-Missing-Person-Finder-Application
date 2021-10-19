8a23b73f1fd6fb55b9087089ca62ffe73f991b9f
Rohitreddz <77590158+Rohitreddz@users.noreply.github.com>
2021-10-18 21:43:15 -0500

    removed complaint button and changes in layout

---
+++ app/src/main/java/com/example/crmp/ComplaintStatusActivity.java
@@ -0,0 +1,14 @@
+package com.example.crmp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ComplaintStatusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_complaint_status);
+    }
+}
