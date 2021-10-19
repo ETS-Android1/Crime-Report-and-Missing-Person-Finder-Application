aada5577132d6cb221033276b95db689b495bc5e
Rohitreddz <77590158+Rohitreddz@users.noreply.github.com>
2021-10-18 22:18:51 -0500

    Added Complaint Status screen

--- app/src/main/java/com/example/crmp/SignInComplaintActivity.java
+++ app/src/main/java/com/example/crmp/SignInComplaintActivity.java
@@ -2,7 +2,9 @@ package com.example.crmp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignInComplaintActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class SignInComplaintActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in_complaint);
+
+    }
+
+    public void ComplaintStatusOnClick(View v) {
+        Intent intent = new Intent(this, ComplaintStatusActivity.class);
+        startActivity(intent);
     }
 }
